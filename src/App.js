2918323aa0bd579da632bda58180bd2f79d9d030
Dawid Bienkowski <marksoft1992@gmail.com>
2019-11-13 08:51:49 +0100

    update component listx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter,Route} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import MainLayout from './components/layout/MainLayout/MainLayout';
+import { AnimatedSwitch } from 'react-router-transition';
 
+import styles from './App.scss';
 import Home from './components/views/Home/Home';
 import Trips from './components/views/Trips/TripsContainer';
 // TODO - import other views
+import Countries from './components/views/Countries/CountriesContainer';
+import Regions from './components/views/Regions/RegionsContainer';
+import Trip from './components/views/Trip/TripContainer';
+import Country from './components/views/Country/CountryContainer';
+
+
 import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
 
@@ -18,7 +26,7 @@ class App extends React.Component {
   static propTypes = {
     trips: PropTypes.array,
     setStates: PropTypes.func,
-  }
+  };
 
   constructor(props){
     super(props);
@@ -37,13 +45,28 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <MainLayout>
-          <Switch location={location}>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/trips' component={Trips} />
-            {/* TODO - add more routes for other views */}
-            <Route exact path='/info' component={Info} />
-            <Route path='*' component={NotFound} />
-          </Switch>
+          <AnimatedSwitch
+            location={location}
+            atEnter={{ opacity: 0, offset: 200 }}
+            atLeave={{ opacity: 0 }}
+            atActive={{ opacity: 1, offset: 0 }}
+            className={styles.switchWrapper}
+            mapStyles={styles => ({
+              opacity: styles.opacity,
+              transform: `translateY(${styles.offset}px)`,
+            })}
+          >
+            <Route exact path="/" component={Home} />
+            <Route exact path="/trips" component={Trips} />
+            {/* DONE - TODO - add more routes for other views */}
+            <Route exact path="/countries" component={Countries} />
+            <Route exact path="/regions" component={Regions} />
+            <Route exact path="/trip/:id" component={Trip} />
+            <Route exact path="/country/:id" component={Country} />
+
+            <Route exact path="/info" component={Info} />
+            <Route path="*" component={NotFound} />
+          </AnimatedSwitch>
         </MainLayout>
       </BrowserRouter>
     );
--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -1,41 +1,54 @@
 /* SELECTORS */
 
-export const getAllTrips = ({trips}) => trips;
+export const getAllTrips = ({ trips }) => trips;
 
-export const getFilteredTrips = ({trips, filters}) => {
+export const getFilteredTrips = ({ trips, filters }) => {
   let output = trips;
 
   // filter by search phrase
-  if(filters.searchPhrase){
+  if (filters.searchPhrase) {
     const pattern = new RegExp(filters.searchPhrase, 'i');
     output = output.filter(trip => pattern.test(trip.name));
   }
 
-  // TODO - filter by duration
+  // DONE - TODO - filter by duration
+  if (filters.duration) {
+    output = output.filter(trip => trip.days >= filters.duration.from && trip.days <= filters.duration.to);
+  }
 
-  // TODO - filter by tags
-
-  // TODO - sort by cost descending (most expensive goes first)
+  // DONE - TODO - filter by tags
+  if (filters.tags) {
+    output = output.filter(trip => filters.tags.every(tag => trip.tags.indexOf(tag) > -1));
+  }
+  // DONE - TODO - sort by cost descending (most expensive goes first)
+  const getValueFromString = costString => {
+    const parsed = parseInt(
+      costString /* string argument to parse*/
+        .split('') /* split a string into an array of substrings (string is split between each character) */
+        .filter(element => !isNaN(element)) /* creates a new array of elements that are numbers (removes all elements that are not numbers) */
+        .join('') /* combines all elements of an array into one string */
+    );
+    return parsed;
+  };
+  output.sort((a, b) => getValueFromString(b.cost) - getValueFromString(a.cost)); /* sorting results (descending) */
 
   return output;
 };
 
-export const getTripById = ({trips}, tripId) => {
-  const filtered = trips;
-
-  // TODO - filter trips by tripId
+export const getTripById = ({ trips }, tripId) => {
+  // DONE - TODO - filter trips by tripId
+  const filtered = trips.filter(trip => trip.id == tripId);
 
   console.log('filtering trips by tripId:', tripId, filtered);
-  return filtered.length ? filtered[0] : {error: true};
+  return filtered.length ? filtered[0] : { error: true };
 };
 
-export const getTripsForCountry = ({trips}, countryCode) => {
-  const filtered = trips;
-
-  // TODO - filter trips by countryCode
+export const getTripsForCountry = ({ trips }, countryCode) => {
+  // DONE - TODO - filter trips by countryCode
+  const filtered = trips.filter(trip => trip.country.code == countryCode);
 
   console.log('filtering trips by countryCode:', countryCode, filtered);
-  return filtered.length ? filtered : [{error: true}];
+  return filtered.length ? filtered : [{ error: true }];
 };
 
 /* ACTIONS */
